489c9c10e99fc70e089dfb98af0eb03923da4936
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-12-05 02:39:55 +0500

    Create struct 1.c

---
+++ struct 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+struct Student {
+    int age;
+    char first_name[51];
+    char last_name[51];
+    int standard;
+};
+
+int main() {
+    struct Student s;
+
+    scanf("%d", &s.age);
+    scanf("%s", s.first_name);
+    scanf("%s", s.last_name);
+    scanf("%d", &s.standard);
+
+    printf("%d %s %s %d\n", s.age, s.first_name, s.last_name, s.standard);
+
+    return 0;
+}
